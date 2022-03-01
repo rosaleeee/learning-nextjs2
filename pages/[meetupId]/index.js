fb5d3a657532ddaa9152a2cd54f4e6a429dc1b99
rosa <rosa.lee@dgmit.com>
2022-03-01 17:06:47 +0900

    Adding Custom Components & Styling With CSS Modules

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,1 +1,16 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
 // our-domain.com/new-meetup/:meetupId
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://images.unsplash.com/photo-1504297050568-910d24c426d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+      title="A First Meetup"
+      address="Some address 5, 12345 Some City"
+      description="This is a first meetup!"
+    />
+  );
+}
+
+export default MeetupDetails;
