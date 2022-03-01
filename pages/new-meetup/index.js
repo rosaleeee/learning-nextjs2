715bdec13a90ab72537298a3a7e3b7e7f908c919
rosa <rosa.lee@dgmit.com>
2022-03-01 14:23:54 +0900

    Adding A Form For adding Meetups

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,13 @@
+import MeetupForm from '../../components/meetups/NewMeetupForm';
+
 // our-domain.com/new-meetup
+
+function NewMeetupPage() {
+  function addMeetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData);
+  }
+
+  return <MeetupForm onAddMeetup={addMeetupHandler} />;
+}
+
+export default NewMeetupPage;
