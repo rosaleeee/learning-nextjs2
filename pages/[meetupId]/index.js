8408653eb655b71ba0fe6f3d17624ae6fd8986ff
rosa <rosa.lee@dgmit.com>
2022-03-02 01:43:20 +0900

    Dynamic Pages & getStaticProps & getStaticPaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -13,20 +13,40 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
 export async function getStaticProps(context) {
   // fetch data for a single meetup
 
   const meetupId = context.params.meetupId;
 
-  console.log(context);
+  console.log(meetupId);
 
   return {
-    meetupData: {
-      image:
-        'https://images.unsplash.com/photo-1504297050568-910d24c426d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-      title: 'A First Meetup',
-      address: 'Some address 5, 12345 Some City',
-      description: 'This is a first meetup!',
+    props: {
+      meetupData: {
+        image:
+          'https://images.unsplash.com/photo-1504297050568-910d24c426d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        title: 'A First Meetup',
+        address: 'Some address 5, 12345 Some City',
+        description: 'This is a first meetup!',
+      },
     },
   };
 }
