8f7416a6a140944a444bca033f93713a787b7270
rosa <rosa.lee@dgmit.com>
2022-03-02 01:26:04 +0900

    Working With Dynamic Path Params In getStaticProps

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -13,4 +13,22 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(context);
+
+  return {
+    meetupData: {
+      image:
+        'https://images.unsplash.com/photo-1504297050568-910d24c426d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+      title: 'A First Meetup',
+      address: 'Some address 5, 12345 Some City',
+      description: 'This is a first meetup!',
+    },
+  };
+}
+
 export default MeetupDetails;
